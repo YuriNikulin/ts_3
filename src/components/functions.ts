ee23691bd073dd916b85996b2ad482f27ba8b6d4
Yuriy Nikulin <kowkogab@gmail.com>
2019-08-28 15:25:12 +0400

    overloaded functions

--- src/components/functions.ts
+++ src/components/functions.ts
@@ -14,5 +14,47 @@ export default (function() {
     }
   }
 
-  console.log(greetNamed('Vasily'));
+  // console.log(greetNamed('Vasily'));
+
+  let add: (foo: number, bar: number, foobar: number) => number;
+
+  add = function add (foo: number, bar: number, foobar: number): number {
+    return foo + bar + foobar;
+  }
+
+  function advancedAdd(numbers: number[]): string {
+    return `${numbers.reduce((prev, current) => {
+      return prev + current;
+    }, 0)}`
+  }
+
+
+
+  console.log(add(1, 5, 7));
+
+  console.log(advancedAdd([5, 34]))
+
+interface OverloadingInterface {
+  someMethod(someParameter: "string"): string;
+  someMethod(someParameter: "number"): number;
+  someMethod(someParameter: boolean): boolean;
+}
+
+class OverloadedClass implements OverloadingInterface {
+  someMethod(param: "string");
+  someMethod(param: "number");
+  someMethod(param: boolean);
+  someMethod(param: "string" | "number" | boolean) {
+    if (param === 'string') {
+      return 'string';
+    } else if (param === 'number') {
+      return '10';
+    } else if (typeof param === 'boolean') {
+      return [1, 5, 10];
+    }
+  }
+}
+
+new OverloadedClass().someMethod('number').toLowerCase();
+
 })();
